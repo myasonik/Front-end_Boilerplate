5d7713f8876ffcb6170f528022e8815b511371db
myasonik <myasonik@mindstreaminteractive.com>
2015-03-04 10:10:31 -0500

    fixes path name

--- gulp/imgs.js
+++ gulp/imgs.js
@@ -4,7 +4,7 @@ var config = require('./config.js');
 var imagemin = require('gulp-imagemin');
 
 $.gulp.task('imgs', function() {
-	$.gulp.src(config.dest + 'imgs/**/*.{png,jpg,jpeg,gif,svg}')
+	$.gulp.src(config.src + 'imgs/**/*.{png,jpg,jpeg,gif,svg}')
 		.pipe($.should(config.prod, imagemin({
 			progressive: true,
 			interlaced: true
