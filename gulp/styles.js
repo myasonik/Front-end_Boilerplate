79da7d9f407d88302feb70cbfa4210ccf9dd79ae
myasonik <myasonik@mindstreaminteractive.com>
2015-05-07 15:57:08 -0400

    futzing

--- gulp/styles.js
+++ gulp/styles.js
@@ -4,8 +4,23 @@ var config = require('./config.js');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
+var styleguide = require('sc5-styleguide');
 
-$.gulp.task('styles', function() {
+var source = config.src + 'styles/*.scss';
+
+$.gulp.task('generate-styleguide', function() {
+    return $.gulp.src(source)
+        .pipe(styleguide.generate({
+            title: 'Boilerplate Styleguide',
+            server: true,
+            port: 3002,
+            rootPath: config.dest + '/styleguide/',
+            overviewPath: 'README.md'
+        }))
+        .pipe($.gulp.dest(config.dest));
+});
+
+$.gulp.task('create-styleguide', ['generate-styleguide'], function() {
     var postpros = [ require('autoprefixer-core')({'browsers': '> 0%'}) ];
     
     if (config.prod) {
@@ -16,7 +31,33 @@ $.gulp.task('styles', function() {
         );
     }
 
-    $.gulp.src(config.src + 'styles/*.scss')
+    return $.gulp.src(source)
+        .pipe($.should(!config.prod, sourcemaps.init()))
+        .pipe(sass({
+            percision: 4,
+            includePaths: ['./node_modules', './git_submodules']
+        }).on('error', $.notify.onError('<%= error.message %>')))
+        .pipe(postcss(postpros))
+        .pipe(styleguide.applyStyles())
+        .pipe($.should(config.prod, $.rename({ suffix: '.min' })))
+        .pipe($.should(!config.prod, sourcemaps.write()))
+        .pipe($.gulp.dest(config.dest + '/styleguide/'));
+});
+
+$.gulp.task('styles', function() {
+    if (config.watch) gulp.start('create-styleguide');
+
+    var postpros = [ require('autoprefixer-core')({'browsers': '> 0%'}) ];
+    
+    if (config.prod) {
+        postpros.push(
+            require('css-mqpacker'),
+            require('postcss-zindex'),
+            require('csswring')({ preserveHacks: true, removeAllComments: true })
+        );
+    }
+
+    $.gulp.src(source)
         .pipe($.should(!config.prod, sourcemaps.init()))
         .pipe(sass({
             percision: 4,
