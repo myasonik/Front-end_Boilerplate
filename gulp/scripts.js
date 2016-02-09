27e3c1261c13c79dc84eb05cfbc20d3de3fd914c
Michail Yasonik <Michail Yasonik>
2016-02-08 20:57:39 -0800

    switching

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,43 +1,27 @@
 var $ = require('./common.js');
 var config = require('./config.js');
+var eslintOptions = require('./eslintOptions.js');
 
 var eslint = require('gulp-eslint');
-var uglify = require('gulp-uglify');
+var rollup = require('rollup-stream');
+var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
-var watchify = require('watchify');
-var streamify = require('gulp-streamify');
-var browserify = require('browserify');
-var collapse = require('bundle-collapser/plugin');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 
 $.gulp.task('lint-scripts', function() {
-    return $.gulp.src(config.src + 'scripts/**/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe($.should(config.prod, eslint.failOnError()))
+  return $.gulp.src(config.src + 'scripts/**/*.js')
+    .pipe(eslint(eslintOptions))
+    .pipe(eslint.format())
+    .pipe($.should(config.prod, eslint.failOnError()))
 });
 
 $.gulp.task('scripts', ['lint-scripts'], function() {
-    function doBrowserify(b) {
-        b.bundle()
-            .on('error', $.notify.onError('<%= error.message %>'))
-            .pipe(source('main.js'))
-            .pipe($.should(config.prod, streamify(uglify())))
-            .pipe($.should(config.prod, $.rename({suffix: '.min'})))
-            .pipe($.gulp.dest(config.dest));
-    }
-
-    var b = browserify({
-        plugin: [collapse],
-        debug: !config.prod
-    }).transform('babelify', {presets: ['es2015']});
-
-    if (config.watch) {
-        b = watchify(b);
-        b.on('update', function() {
-            doBrowserify(b);
-        });
-    }
-
-    b.add('./' + config.src + '/scripts/main.js')
-    doBrowserify(b);
+  return rollup({entry: './' + config.src + '/scripts/main.js'})
+    // .on('error', $.notify.onError('<%= error.message %>'))
+    .pipe(source('main.js'))
+    // .pipe(buffer())
+    // .pipe($.should(!config.prod, sourcemaps.init({loadMaps: true})))
+    // .pipe($.should(!config.prod, sourcemaps.write('.')))
+    .pipe($.gulp.dest(config.dest));
 });
